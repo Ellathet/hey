c679aae738e0874cf2743e7be974e486235d52d8
Mikha Thet <social.amais@gmail.com>
2022-10-21 21:07:32 -0300

    chore: http options

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,25 @@
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
+
+// logger middleware
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+// security options for http
+app.use(helmet())
+
+// request body parser
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}))
+
+// cors options
+app.use(cors())
 
 app.listen(3025, () => console.log(3025))
